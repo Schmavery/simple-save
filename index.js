21fbc3370040624539fb7b888d81d4fd1327cb94
Schmavery <avery.schmavery@gmail.com>
2015-09-10 23:33:54 -0700

    More regex escaping and function toString

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 var fs = require("fs");
 delete require.cache[__filename];
 
-var escapeRegex = function(s) {
+function escapeRegex(s) {
   if (s) return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+
+function escapeDollar(s) {
+  if (s) return s.replace(/[$]/g, '$$$$');
 };
 
 module.exports = function (pattern) {
   var PATTERN = pattern || "SS";
   return function(name, value, suffix) {
-    //console.log("Changing "+name+" to `" + value + "`");
     name = escapeRegex(name);
     suffix = escapeRegex(suffix);
+    if (typeof value === "function"){
+	value = value.toString();
+    } else {
+    	value = JSON.stringify(value);
+    }
+    value = escapeDollar(value);
+    //console.log("Changing "+name+" to `" + value + "`");
     fs.readFile(module.parent.filename, "utf-8", function(err, data) {
       if(err) throw err;
       var match = "(\\/\\*" + PATTERN +
         (suffix ? ":" + suffix : "") +
         "\\*\\/.*?" + name + ".*?=).*;";
-      var replace = '$1 '+JSON.stringify(value)+";";
+      var replace = '$1 '+value+";";
+      //console.log(RegExp(match, "gm").exec(data));
       data = data.replace(RegExp(match, "gm"), replace);
       fs.writeFile(module.parent.filename, data, function(err) {
         if (err) throw err;
